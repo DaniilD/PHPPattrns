72ef0ecee3a8ceaa029d6dad265d5047704b3699
Daniil <daniildomrahev@gmail.com>
2020-08-22 19:28:37 +0500

    Реализовал паттерн компоновщик

---
+++ app/Patterns/Structural/Composite/Fieldset.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Patterns\Structual\Composite;
+
+/**
+ * Элемент fieldset представляет собой Конкретный Контейнер.
+ */
+class Fieldset extends FieldComposite
+{
+    public function render(): string
+    {
+        // Обратите внимание, как комбинированный результат рендеринга потомков
+        // включается в тег fieldset.
+        $output = parent::render();
+
+        return "<fieldset><legend>{$this->title}</legend>\n$output</fieldset>\n";
+    }
+}

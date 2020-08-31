ca7289aeaa73d941a463dca700908d26ebe38f68
Daniil <daniildomrahev@gmail.com>
2020-08-31 09:02:48 +0500

    Добавил реализацию паттерна декоратор

---
+++ app/Patterns/Structural/Decorator/InputFormat.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\Patterns\Structual\Decorator;
+
+/**
+ * Интерфейс Компонента объявляет метод фильтрации, который должен быть
+ * реализован всеми конкретными компонентами и декораторами.
+ */
+interface InputFormat
+{
+    public function formatText(string $text): string;
+}

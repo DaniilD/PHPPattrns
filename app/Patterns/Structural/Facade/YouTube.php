0d687397754937ce51b25902efd142d9f731ade0
Daniil <daniildomrahev@gmail.com>
2020-09-14 07:55:59 +0500

    Реализовал паттерн Facade

---
+++ app/Patterns/Structural/Facade/YouTube.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Подсистема API YouTube.
+ */
+class YouTube
+{
+    public function fetchVideo(): string { /* ... */ }
+
+    public function saveAs(string $path): void { /* ... */ }
+
+    // ...дополнительные методы и классы...
+}
